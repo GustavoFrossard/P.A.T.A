f146c6c5052a0a4fdb96a4136a479a506e26a2f0
GustavoFrossard <gustavo.fbr.dearaujo@gmail.com>
2025-11-14 00:27:39 -0300

    fix: add app variavel

--- Projeto/RoveriBack_final/project/wsgi.py
+++ Projeto/RoveriBack_final/project/wsgi.py
@@ -1,4 +1,8 @@
 import os
 from django.core.wsgi import get_wsgi_application
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 application = get_wsgi_application()
+
+# Vercel needs this
+app = application
